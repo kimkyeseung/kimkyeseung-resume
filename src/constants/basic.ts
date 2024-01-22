ce4786d5ef79a85bdf3fbaa69c97ca2ab815c606
Kimkyeseung <chickenboys@naver.com>
2024-01-22 17:04:48 +0900

    ♻️ 코드 구조 개선

---
+++ src/constants/basic.ts
@@ -0,0 +1,5 @@
+export const DOCUMENT_TITLE = '프론트엔드 개발자 김계승 이력서'
+export const EMAIL = 'kimkyeseung@gmail.com'
+export const PHONE_NUMBER = '010 - 2848 - 9508'
+export const GITHUB_URL = 'https://github.com/kimkyeseung'
+export const ADDRESS = '서울 영등포구 당산동'
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,13 +1,5 @@
-import { Education, Experience } from '@/types'
-
-export const DOCUMENT_TITLE = '프론트엔드 개발자 김계승 이력서'
-
-export const EMAIL = 'kimkyeseung@gmail.com'
-export const PHONE_NUMBER = '010 - 2848 - 9508'
-export const GITHUB_URL = 'https://github.com/kimkyeseung'
-export const ADDRESS = '서울 영등포구 당산동'
-
-export const DISPLAY_DATE_FORAMT = 'YYYY.MM'
+export * from './basic'
+export * from './config'
 export * from './experiences'
 export * from './educations'
 export * from './skillsets'
