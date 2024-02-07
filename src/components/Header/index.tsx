6fef63e9fb9d70ef3d588d704f95a4c224bff64a
Kimkyeseung <chickenboys@naver.com>
2024-02-07 19:10:03 +0900

    ✨ 헤더 - 타이틀 서브타이블에 한글 해석 추가

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,22 @@
-import { SUB_TITLE, TITLE } from '@/constants'
+import {
+  SUB_TITLE,
+  TITLE,
+  TRANSLATED_SUB_TITLE,
+  TRANSLATED_TITLE,
+} from '@/constants'
 
 export const Header = () => {
   return (
     <div className="flex flex-col gap-1 md:gap-2">
-      <h1 className="text-title-mobile md:text-title">{TITLE}</h1>
-      <h2 className="text-subtitle-mobile md:text-subtitle">{SUB_TITLE}</h2>
+      <h1 className="text-title-mobile md:text-title" title={TRANSLATED_TITLE}>
+        {TITLE}
+      </h1>
+      <h2
+        className="text-subtitle-mobile md:text-subtitle"
+        title={TRANSLATED_SUB_TITLE}
+      >
+        {SUB_TITLE}
+      </h2>
     </div>
   )
 }
--- src/constants/basic.ts
+++ src/constants/basic.ts
@@ -1,7 +1,10 @@
-export const DOCUMENT_TITLE = '프론트엔드 개발자 김계승 이력서'
+export const DOCUMENT_TITLE = '웹 풀스택 개발자 김계승 이력서'
 export const TITLE = 'Kim Kyeseung'
+export const TRANSLATED_TITLE = '김계승'
 export const SUB_TITLE =
   'Full Stack Web Developer with Strong Frontend and Backend Skills'
+export const TRANSLATED_SUB_TITLE =
+  '프론트엔드와 백엔드 기술에 능숙한 풀스택 웹 개발자'
 export const EMAIL = 'kimkyeseung@gmail.com'
 export const PHONE_NUMBER = '010 - 2848 - 9508'
 export const GITHUB_URL = 'https://github.com/kimkyeseung'
