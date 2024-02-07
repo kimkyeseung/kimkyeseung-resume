93fa93d29bd5fc1f3a06b8d53554bfbdf4f49fa4
Kimkyeseung <chickenboys@naver.com>
2024-02-07 18:37:52 +0900

    💬 헤더 및 타이틀 변경, 상수처리

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,10 @@
+import { SUB_TITLE, TITLE } from '@/constants'
+
 export const Header = () => {
   return (
-    <div className="flex flex-col gap-1 md:gap-4">
-      <h1 className="text-title-mobile md:text-title">Kim Kyeseung</h1>
-      <h2 className="text-subtitle-mobile md:text-subtitle">
-        Frontend Developer
-      </h2>
+    <div className="flex flex-col gap-1 md:gap-2">
+      <h1 className="text-title-mobile md:text-title">{TITLE}</h1>
+      <h2 className="text-subtitle-mobile md:text-subtitle">{SUB_TITLE}</h2>
     </div>
   )
 }
--- src/constants/basic.ts
+++ src/constants/basic.ts
@@ -1,4 +1,7 @@
 export const DOCUMENT_TITLE = '프론트엔드 개발자 김계승 이력서'
+export const TITLE = 'Kim Kyeseung'
+export const SUB_TITLE =
+  'Full Stack Web Developer with Strong Frontend and Backend Skills'
 export const EMAIL = 'kimkyeseung@gmail.com'
 export const PHONE_NUMBER = '010 - 2848 - 9508'
 export const GITHUB_URL = 'https://github.com/kimkyeseung'
